e8867d65e8654b927009a6e7391685e675ed247e
Sebi Cirdeiu <sebbcirdeiu@yahoo.com>
2023-11-04 13:53:27 +0200

    Fix path to cv

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -23,7 +23,7 @@ function Aside() {
                     </a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f79]'> 
-                    <a  className='flex justify-between items-center w-full  text-gray-300' href='personal-website/src/assets/CV/Sebi_Cirdeiu.pdf' download>
+                    <a  className='flex justify-between items-center w-full  text-gray-300' href='./src/assets/CV/Sebi_Cirdeiu.pdf' download>
                         Resume <BsFillPersonLinesFill size={30}/>
                     </a>
                 </li>
